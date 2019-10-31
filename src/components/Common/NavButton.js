4c8eb688ffce59c3292d70c70ff68d1a9cc3e95f
roberth <roberthchirinos87@gmail.com>
2019-10-31 08:08:35 +0200

    edited package json

--- src/components/Common/NavButton.js
+++ src/components/Common/NavButton.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Common/Button';
-import cx from 'classnames';
 
 const proptypes = {
   type: PropTypes.string,
