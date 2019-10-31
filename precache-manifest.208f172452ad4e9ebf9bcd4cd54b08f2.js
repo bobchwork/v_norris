14529f4dba3e3fca56df2eb756b40bbbd1b288cd
roberth <roberthchirinos87@gmail.com>
2019-10-31 08:09:35 +0200

    Updates

---
+++ precache-manifest.208f172452ad4e9ebf9bcd4cd54b08f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d732df9167ab0fbc6f1dff087134093",
+    "url": "/v_norris/index.html"
+  },
+  {
+    "revision": "5778ff75be7df3580b36",
+    "url": "/v_norris/static/css/main.3738920d.chunk.css"
+  },
+  {
+    "revision": "32dc4d2b8e546c1b0dc0",
+    "url": "/v_norris/static/js/2.088ef72a.chunk.js"
+  },
+  {
+    "revision": "5778ff75be7df3580b36",
+    "url": "/v_norris/static/js/main.a4c27924.chunk.js"
+  },
+  {
+    "revision": "ed44c54cfbe67fd531da",
+    "url": "/v_norris/static/js/runtime-main.5f0bf96d.js"
+  },
+  {
+    "revision": "abb742ccfc6baa414f0e91d382f3abef",
+    "url": "/v_norris/static/media/blue-light.abb742cc.svg"
+  },
+  {
+    "revision": "17c4f721b27a36036c09fa0ddb0a3181",
+    "url": "/v_norris/static/media/footer-bg.17c4f721.png"
+  },
+  {
+    "revision": "d02324cac9b23b1e40e4ea8a4ecf7ab8",
+    "url": "/v_norris/static/media/main-bg.d02324ca.png"
+  },
+  {
+    "revision": "212cfdfd3c89f13761ee580ec9c7001e",
+    "url": "/v_norris/static/media/navbutton-bg.212cfdfd.svg"
+  }
+]);
